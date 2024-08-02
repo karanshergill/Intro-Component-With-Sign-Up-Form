2258e1d03f39cd7a54aab9b8b832960096f78288
Karan <email@karanshergill.com>
2024-08-02 12:30:09 +0530

    fix css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,12 +5,16 @@ export const Home = () => {
     return (
         <section className='w-3/4'>
             <div className='grid md:grid-cols-2'>
-                <div className='flex flex-col px-4 gap-8'>
+                <div className='flex flex-col px-4 gap-8 justify-center'>
                     <h1 className='text-5xl font-semibold leading-[54px] text-white'>Learn to code by watching others</h1>
                     <p className='text-white'>See how experienced developers solve problems in real-time. Watching scripted tutorials is great,
                         but understanding how developers think is invaluable.</p>
                 </div>
-                <div>
+                <div className='flex flex-col ml-10'>
+                    <div className='bg-color-blue text-center py-3.5 rounded-lg'>
+                        <p className='text-sm text-color-grayish-blue'><span className='font-semibold text-white'>Try it free 7 days</span> then $20/mo. thereafter</p>
+                    </div>
+                    <div className='bg-color-blue/30 -mt-1 mb-4 h-2.5 rounded-b-lg'></div>
                     <TrialSignUpForm />
                 </div>
             </div>
--- src/components/TrialSignUpForm.jsx
+++ src/components/TrialSignUpForm.jsx
@@ -1,17 +1,67 @@
-import React from 'react'
-  
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
 export const TrialSignUpForm = () => {
+
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
+  // console.log(watch("firstName"));
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit(onSubmit)} className='bg-white p-10 rounded-lg'>
       <div className="mb-4">
         <input
           type="text"
           placeholder="First Name"
-        //   {...register('firstName', { required: 'First Name cannot be empty' })}
-        //   className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-purple-500 ${errors.firstName ? 'border-red-500' : ''}`}
-        // 
+          {...register('firstName', { required: 'First Name cannot be empty' })}
+          className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-color-dark-blue placeholder:text-sm placeholder:font-semibold placeholder:text-color-dark-blue/70 ${errors.firstName ? 'border-red-500' : ''}`}
         />
-        {/* {errors.firstName && <p className="text-red-500 text-sm mt-1">{errors.firstName.message}</p>} */}
+        {errors.firstName && <p className="text-right text-xs font-semibold italic mt-2 text-color-red">{errors.firstName.message}</p>}
+      </div>
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Last Name"
+          {...register('lastName', { required: 'Last Name cannot be empty' })}
+          className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-color-dark-blue placeholder:text-sm placeholder:font-semibold placeholder:text-color-dark-blue/70 ${errors.firstName ? 'border-red-500' : ''}`}
+        />
+        {errors.firstName && <p className="text-right text-xs font-semibold italic mt-2 text-color-red">{errors.lastName.message}</p>}
+      </div>
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Email Address"
+          {...register('email', {
+            required: 'Email cannot be empty',
+            pattern: {
+              value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+              message: 'Looks like this is not an email'
+            }
+          })}
+          className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-color-dark-blue placeholder:text-sm placeholder:font-semibold placeholder:text-color-dark-blue/70 ${errors.email ? 'border-red-500' : ''}`}
+        />
+        {errors.email && <p className="text-right text-xs font-semibold italic mt-2 text-color-red">{errors.email.message}</p>}
+      </div>
+      <div className="mb-4">
+        <input
+          type="password"
+          placeholder="Password"
+          {...register('password', {
+            required: 'Password cannot be empty'
+          })}
+          className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-color-dark-blue placeholder:text-sm placeholder:font-semibold placeholder:text-color-dark-blue/70 ${errors.password ? 'border-red-500' : ''}`}
+        />
+        {errors.password && <p className="text-right text-xs font-semibold italic mt-2 text-color-red">{errors.password.message}</p>}
+      </div>
+      <button
+        type="submit"
+        className="w-full bg-color-green text-white text-sm py-4 rounded-lg hover:bg-green-600 transition duration-300"
+      >
+        CLAIM YOUR FREE TRIAL
+      </button>
+      <div className='text-xs text-center text-color-grayish-blue m-4'>
+        <p>By clicking the button, you are agreeing to our <span className='text-color-red font-semibold'>Terms and Services</span></p>
       </div>
     </form>
   )
